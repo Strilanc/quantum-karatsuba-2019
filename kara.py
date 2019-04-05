2fd7d0d3337491e67ffd1674559c5343064ce002
Craig Gidney <craiggidney@google.com>
2019-04-05 15:14:12 -0700

    Flailing

--- kara.py
+++ kara.py
@@ -1,4 +1,6 @@
 import math
+import random
+from typing import Iterable, Sequence, List, Tuple, Any
 
 from .int_buffer import IntBuf, RawIntBuffer, RawWindowBuffer, RawConcatBuffer
 
@@ -9,60 +11,356 @@ def ceil_power_of_2(n: int) -> int:
     return 1 << int(math.ceil(math.log2(n)))
 
 
+def popcnt(n: int) -> int:
+    assert n >= 0
+    t = 0
+    while n:
+        n &= n-1
+        t += 1
+    return t
+
+
+def adjacent_pairs(seq: Sequence[int]) -> List[Tuple[int, int]]:
+    return [(seq[i], seq[i+1]) for i in range(len(seq) - 1)]
+
+
+def hamming_seq(n: int) -> bool:
+    return bool(popcnt(n) & 1)
+
+
 def add_square_into(val: IntBuf,
                     out: IntBuf,
                     pos: bool = True,
-                    n: int = None):
+                    n: int = None,
+                    factors: int = 0,
+                    merges: int = 0,
+                    position: int = 0,
+                    results = None):
     if n is None:
         n = ceil_power_of_2(len(val))
     n >>= 1
     if n <= 2:
+        if results is not None:
+            results.append((factors, merges, position))
         out += (+1 if pos else -1) * int(val)**2
-        return
-
-    p = 4
-    m = n
-    while m >= p:
-        times_mul_inverse_1k1(m, out)
-        m >>= 1
-
-    seq = [True]
-    for i in range(0, n * 2, p):
-        if i//p >= len(seq):
-            seq = seq + [not e for e in seq]
-        sign = +1 if seq[i//p] == pos else -1
-        out[i:] += int(val[i:i+p])**2 * sign
-
-    m = n
-    while m >= p:
-        out[m:] -= out
-        m >>= 1
-
-    add_rem_square_into(val, out, pos, n)
-
-
-def add_rem_square_into(val: IntBuf,
-                    out: IntBuf,
-                    pos: bool,
-                    n: int):
-    if n <= 2:
         return
 
     a = val[:n]
     b = val[n:]
     times_mul_inverse_1k1(n, out)
-    add_rem_square_into(a, out, pos, n >> 1)
-    add_rem_square_into(b, out[n:], not pos, n >> 1)
+    add_square_into(a, out, pos, n, factors + n, merges, position, results)
+    add_square_into(b, out[n:], not pos, n, factors + n, merges, position + n, results)
     out[n:] -= out[:]
 
     c = a.padded(1)
     c += b
-    add_square_into(a, out[n:], pos, n)
+    add_square_into(a, out[n:], pos, n, factors, merges + n, position + n, results)
     if c[-1]:
         m = len(c)
-        out[n+m:] += int(a) * (+1 if pos else -1)
-        out[n+2*m-2:] += +1 if pos else -1
+        out[n + m:] += int(a) * (+1 if pos else -1)
+        out[n + 2 * m - 2:] += +1 if pos else -1
     c -= b
+
+
+class MutableInt:
+    def __init__(self, val):
+        self.val = val
+
+    def __int__(self):
+        return self.val
+
+    def __eq__(self, other):
+        if not isinstance(other, (MutableInt, int)):
+            return int(self) == int(other)
+        return NotImplemented
+
+    def __repr__(self):
+        return str(self.val)
+
+
+def split_into_pieces(value: int, piece_size: int, piece_count: int) -> List[MutableInt]:
+    assert value.bit_length() <= piece_size * piece_count
+    mask = ~(-1 << piece_size)
+    result = []
+    for i in range(0, piece_count * piece_size, piece_size):
+        result.append(MutableInt((value >> i) & mask))
+    return result
+
+
+def fuse_pieces(pieces: List[MutableInt], piece_size: int) -> int:
+    result = 0
+    for i, p in enumerate(pieces):
+        result += p.val << (i * piece_size)
+    return result
+
+
+def add_into_pieces(input_pieces: List[MutableInt],
+                    output_pieces: List[MutableInt],
+                    factor: int = 1):
+    assert len(input_pieces) == len(output_pieces)
+    for i in range(len(input_pieces)):
+        output_pieces[i].val += input_pieces[i].val * factor
+
+
+def add_square_into_pieces(input_pieces: List[MutableInt],
+                           output_pieces: List[MutableInt],
+                           pos: bool = True):
+    assert len(input_pieces) * 2 == len(output_pieces)
+    if len(input_pieces) == 1:
+        output_pieces[0].val += input_pieces[0].val**2 * (+1 if pos else -1)
+        return
+    h = len(input_pieces) >> 1
+
+    for i in range(h, len(output_pieces)):
+        output_pieces[i].val += output_pieces[i - h].val
+    add_square_into_pieces(input_pieces=input_pieces[:h],
+                           output_pieces=output_pieces[:2*h],
+                           pos=pos)
+    add_square_into_pieces(input_pieces=input_pieces[h:],
+                           output_pieces=output_pieces[h:3*h],
+                           pos=not pos)
+    for i in range(h, len(output_pieces))[::-1]:
+        output_pieces[i].val -= output_pieces[i - h].val
+
+    add_into_pieces(input_pieces[:h], input_pieces[h:])
+    add_square_into_pieces(input_pieces=input_pieces[h:],
+                           output_pieces=output_pieces[h:3*h],
+                           pos=pos)
+    add_into_pieces(input_pieces[:h], input_pieces[h:], factor=-1)
+
+
+# def add_square_into_pieces(input_pieces: List[int],
+#                            output_pieces: List[int]):
+#     if n is None:
+#         n = ceil_power_of_2(len(val))
+#     n >>= 1
+#     if n <= 2:
+#         if results is not None:
+#             results.append((factors, merges, position))
+#         out += (+1 if pos else -1) * int(val)**2
+#         return
+#
+#     a = val[:n]
+#     b = val[n:]
+#     times_mul_inverse_1k1(n, out)
+#     add_square_into(a, out, pos, n, factors + n, merges, position, results)
+#     add_square_into(b, out[n:], not pos, n, factors + n, merges, position + n, results)
+#     out[n:] -= out[:]
+#
+#     c = a.padded(1)
+#     c += b
+#     add_square_into(a, out[n:], pos, n, factors, merges + n, position + n, results)
+#     if c[-1]:
+#         m = len(c)
+#         out[n + m:] += int(a) * (+1 if pos else -1)
+#         out[n + 2 * m - 2:] += +1 if pos else -1
+#     c -= b
+
+
+def new_bits(*, old: int, new: int) -> List[int]:
+    assert old >= 0 and new >= 0
+    t = 0
+    result = []
+    while old or new:
+        if (new & 1) and not (old & 1):
+            result.append(t)
+        old <<= 1
+        new <<= 1
+        t += 1
+    return result
+
+
+def scatter(bits: int, mask: int) -> int:
+    t = 0
+    while mask:
+        new_mask = mask & (mask - 1)
+        out = new_mask ^ mask
+        if bits & 1:
+            t |= out
+        bits >>= 1
+        mask = new_mask
+    return t
+
+
+def set_bit_vals(mask: int) -> List[int]:
+    result = []
+    while mask:
+        new_mask = mask & (mask - 1)
+        result.append(new_mask ^ mask)
+        mask = new_mask
+    return result
+
+
+def mask_iter(mask: int) -> List[int]:
+    result = [0]
+    for v in set_bit_vals(mask):
+        result += [e + v for e in result]
+    return result
+
+
+def matchers(only: int) -> List[int]:
+    return [p for p in range(only + 1) if p == p | only]
+
+
+def filter_position_matches(sequence: List[Any], mask: int) -> List[Any]:
+    return [e for i, e in enumerate(sequence) if i == (i & mask)]
+
+
+def power_of_two_ness(v: int) -> int:
+    assert v > 0
+    return v ^ (v & (v - 1))
+
+
+def ceil_lg2(n: int):
+    return int(math.ceil(math.log2(n)))
+
+
+def add_square_into_alt(val: IntBuf, out: IntBuf):
+    n = ceil_power_of_2(max(len(val), len(out)))
+    mask = (n - 1) >> 2
+    s = ceil_lg2(mask)
+    prev_m = 0
+
+    p = 4
+    v0 = int(val)
+    in_pieces = [val[i:i+p].padded(10) for i in range(0, n, p)]
+    out_pieces = [IntBuf.zero(2*p+10) for _ in range(0, n, p)]
+    for k in out_pieces:
+        k[:] = random.randint(0, 1 << 100)
+    o0 = [int(o) for o in out_pieces]
+    os = []
+
+    xx = mask
+    for b in range(s)[::-1]:
+        v = 1 << b
+        os.append([int(o) for o in out_pieces])
+        print('REMOVE', xx, v, os[-1])
+        for p in mask_iter(xx):
+            if p + v < len(out_pieces):
+                print(p+v, '<-', p, end=' ')
+                out_pieces[p + v] -= out_pieces[p]
+        print()
+        xx ^= v
+
+    for m in range(mask + 1):
+        # Transition pieces
+        if m:
+            k = ceil_lg2(power_of_two_ness(m))
+            for r in range(k):
+                v = 1 << r
+                os.append([int(o) for o in out_pieces])
+                print('REMOVE', v, os[-1])
+                for p in mask_iter(prev_m ^ mask):
+                    if p + v < len(out_pieces):
+                        print(p + v, '<-', p, end=' ')
+                        out_pieces[p + v] += out_pieces[p]
+                print()
+                for p in mask_iter(prev_m)[::-1]:
+                    if p + v < len(in_pieces):
+                        in_pieces[p + v] -= in_pieces[p]
+                prev_m ^= v
+
+            v = 1 << k
+            prev_m ^= v
+            for p in mask_iter(prev_m ^ mask)[::-1]:
+                if p + v < len(out_pieces):
+                    print(p, '+>', p+v, end=' ')
+                    out_pieces[p + v] += out_pieces[p]
+            print()
+            print('APPEND', prev_m ^ mask, v, [int(o) for o in out_pieces])
+            assert os.pop() == [int(o) for o in out_pieces]
+            for p in mask_iter(prev_m):
+                if p + v < len(in_pieces):
+                    in_pieces[p + v] += in_pieces[p]
+
+        # Perform squaring work.
+        # for p in range(mask):
+        #     if p == p | m:
+        #         out_pieces[p] += int(in_pieces[p])**2
+
+    # Unfuse.
+    for b in range(s):
+        v = 1 << b
+        for p in mask_iter(prev_m)[::-1]:
+            if p + v < len(in_pieces):
+                in_pieces[p + v] -= in_pieces[p]
+        prev_m ^= v
+
+    assert v0 == int(val)
+    assert o0 == [int(o) for o in out_pieces]
+
+
+# def add_square_into(val: IntBuf,
+#                     out: IntBuf,
+#                     pos: bool = True,
+#                     n: int = None):
+#     if n is None:
+#         n = ceil_power_of_2(len(val))
+#     n >>= 1
+#     if n <= 2:
+#         out += (+1 if pos else -1) * int(val)**2
+#         return
+#
+#     p = 4
+#     m = n
+#     while m >= p:
+#         times_mul_inverse_1k1(m, out)
+#         m >>= 1
+#
+#     rng = range(0, n * 2, p)
+#     positives = [i for i in rng if hamming_seq(i) ^ pos]
+#     negatives = [i for i in rng if not hamming_seq(i) ^ pos]
+#     positives.append(n * 2)
+#     negatives.append(n * 2)
+#
+#     out *= -1  # Need implicit sign extension to avoid this.
+#     buf = IntBuf.zero(0)
+#     for i, j in adjacent_pairs(negatives):
+#         r = out[i:j].padded(max(1, 2*p - (j-i)))
+#         r += int(val[i:i+p])**2
+#         r += buf
+#         buf = r[j-i:]
+#     out[2*n:] += buf
+#     out *= -1
+#
+#     buf = IntBuf.zero(0)
+#     for i, j in adjacent_pairs(positives):
+#         r = out[i:j].padded(max(1, 2*p - (j-i)))
+#         r += int(val[i:i+p])**2
+#         r += buf
+#         buf = r[j-i:]
+#     out[2*n:] += buf
+#
+#     m = n
+#     while m >= p:
+#         out[m:] -= out
+#         m >>= 1
+#
+#     add_rem_square_into(val, out, pos, n)
+#
+#
+# def add_rem_square_into(val: IntBuf,
+#                     out: IntBuf,
+#                     pos: bool,
+#                     n: int):
+#     if n <= 2:
+#         return
+#
+#     a = val[:n]
+#     b = val[n:]
+#     times_mul_inverse_1k1(n, out)
+#     add_rem_square_into(a, out, pos, n >> 1)
+#     add_rem_square_into(b, out[n:], not pos, n >> 1)
+#     out[n:] -= out[:]
+#
+#     c = a.padded(1)
+#     c += b
+#     add_square_into(a, out[n:], pos, n)
+#     if c[-1]:
+#         m = len(c)
+#         out[n+m:] += int(a) * (+1 if pos else -1)
+#         out[n+2*m-2:] += +1 if pos else -1
+#     c -= b
 
 
 def times_mul_inverse_1k1(n: int, out: IntBuf):
@@ -79,3 +377,23 @@ def times_mul_inverse_1k1(n: int, out: IntBuf):
         r += buf
         r += pieces[i - 1]
         buf = r[n:]
+
+
+# def times_mul_inverse_1k1(pieces: List[int], reduction_mask: int, target_bit: int):
+#     for p in range(len(pieces)):
+#         if reduction_mask & p != reduction_mask:
+#             continue
+#         pieces[i + (1 << target_bit)]
+#     if not len(out):
+#         return
+#     p = int(math.ceil(math.log(len(out) / n)))
+#     pieces = []
+#     for i in range(0, len(out), n):
+#         pieces.append(out[i:i+n])
+#
+#     buf = IntBuf.zero(0)
+#     for i in range(1, len(pieces)):
+#         r = pieces[i].padded(p)
+#         r += buf
+#         r += pieces[i - 1]
+#         buf = r[n:]
--- kara_test.py
+++ kara_test.py
@@ -1,7 +1,7 @@
 import random
 
 from .int_buffer import IntBuf
-from .kara import add_square_into
+from .kara import add_square_into, mask_iter, set_bit_vals, power_of_two_ness, ceil_lg2, split_into_pieces, fuse_pieces, add_into_pieces, add_square_into_pieces
 
 
 def test_add_square_into_small():
@@ -17,12 +17,104 @@ def test_add_square_into_small():
 
 
 def test_add_square_into_large():
-    n = 200
-    x = random.randint(0, 2**n - 1)
-    y = random.randint(0, 2**n - 1)
+    for n in [16, 32, 64, 128]:
+        x = random.randint(0, 2**n - 1)
+        y = random.randint(0, 2**n - 1)
+        val = IntBuf.zero(n)
+        out = IntBuf.zero(n)
+        val[:] = x
+        out[:] = y
+        add_square_into(val, out)
+        assert out == (y + x**2) % 2**n
+
+
+def test_known_failures():
+    n = 128
+    x = 309859417082872587300573064293124092539
+    y = 107908178516259704616406393704185964006
     val = IntBuf.zero(n)
     out = IntBuf.zero(n)
     val[:] = x
     out[:] = y
     add_square_into(val, out)
     assert out == (y + x**2) % 2**n
+
+    # xx = []
+    # add_square_into(val, out, results=xx)
+    # def f(r):
+    #     return bin(r)[2:-2].rjust(10, '_').replace('0', '_')
+    # for factors, merges, position in sorted(xx, key=lambda e: (e[1], e[0])):
+    #     print('f' + f(factors), 'm' + f(merges), 'p' + f(position))
+    # assert False
+
+
+def test_piecewise_addition():
+    r1 = random.randint(0, ~(-1 << 100))
+    r2 = random.randint(0, ~(-1 << 100))
+    p1 = split_into_pieces(r1, 10, 10)
+    p2 = split_into_pieces(r2, 10, 10)
+    add_into_pieces(input_pieces=p1, output_pieces=p2)
+    assert fuse_pieces(p1, 10) == r1
+    assert fuse_pieces(p2, 10) == r1 + r2
+
+
+def test_piecewise_square_addition():
+    piece_size = 16
+    piece_count = 32
+    r1 = random.randint(0, ~(-1 << (piece_count*piece_size)))
+    r2 = random.randint(0, ~(-1 << (piece_count*piece_size*2)))
+    p1 = split_into_pieces(r1, piece_size=piece_size, piece_count=piece_count)
+    p2 = split_into_pieces(r2, piece_size=piece_size, piece_count=piece_count*2)
+    q1 = list(p1)
+    add_square_into_pieces(input_pieces=p1, output_pieces=p2)
+    assert q1 == p1
+    assert fuse_pieces(p1, piece_size) == r1
+    expected = r1**2 + r2
+    actual = fuse_pieces(p2, piece_size)
+    assert actual == expected, (actual, expected, r2, '+', r1**2)
+
+
+def test_set_bit_vals():
+    assert set_bit_vals(0) == []
+    assert set_bit_vals(1) == [1]
+    assert set_bit_vals(2) == [2]
+    assert set_bit_vals(3) == [1, 2]
+    assert set_bit_vals(4) == [4]
+    assert set_bit_vals(5) == [1, 4]
+    assert set_bit_vals(6) == [2, 4]
+    assert set_bit_vals(7) == [1, 2, 4]
+    assert set_bit_vals(8) == [8]
+    assert set_bit_vals(9) == [1, 8]
+
+
+def test_mask_iter():
+    assert mask_iter(0) == [0]
+    assert mask_iter(1) == [0, 1]
+    assert mask_iter(2) == [0, 2]
+    assert mask_iter(3) == [0, 1, 2, 3]
+    assert mask_iter(4) == [0, 4]
+    assert mask_iter(5) == [0, 1, 4, 5]
+
+
+def test_power_of_two_ness():
+    assert power_of_two_ness(1) == 1
+    assert power_of_two_ness(2) == 2
+    assert power_of_two_ness(3) == 1
+    assert power_of_two_ness(4) == 4
+    assert power_of_two_ness(5) == 1
+    assert power_of_two_ness(6) == 2
+    assert power_of_two_ness(7) == 1
+    assert power_of_two_ness(8) == 8
+    assert power_of_two_ness(9) == 1
+
+
+def test_ceil_lg2():
+    assert ceil_lg2(1) == 0
+    assert ceil_lg2(2) == 1
+    assert ceil_lg2(3) == 2
+    assert ceil_lg2(4) == 2
+    assert ceil_lg2(5) == 3
+    assert ceil_lg2(6) == 3
+    assert ceil_lg2(7) == 3
+    assert ceil_lg2(8) == 3
+    assert ceil_lg2(9) == 4
