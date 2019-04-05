4c3cdeea7280eb9f4d41c825afcafdfc96394788
Craig Gidney <craiggidney@google.com>
2019-04-04 21:31:29 -0700

    Linear inv mul

--- kara.py
+++ kara.py
@@ -1,7 +1,13 @@
+import math
+
 from .int_buffer import IntBuf
 
 
-def add_square_into(val: IntBuf, out: IntBuf, pos: bool = True, pad: int = 0, pad_fill: int = 0):
+def add_square_into(val: IntBuf,
+                    out: IntBuf,
+                    pos: bool = True,
+                    pad: int = 0,
+                    pad_fill: int = 0):
     n = (len(val) - pad) >> 1
     if n <= 2:
         if pos:
@@ -12,10 +18,18 @@ def add_square_into(val: IntBuf, out: IntBuf, pos: bool = True, pad: int = 0, pa
 
     a = val[:n]
     b = val[n:]
-    out *= modinv(2**n - 1, 1 << len(out))
-    add_square_into(a, out, not pos, 0, pad_fill)
+    times_mul_inverse_1k1(n, out)
+    add_square_into(a, out, pos, 0, pad_fill)
+    add_square_into(b, out[n:], not pos, pad, pad_fill)
+    out[n:] -= out[:]
+    assert len(b) >= len(a)
+    pad_fill += 1
+    if pad_fill >= 1 << pad:
+        pad += 1
+        b = b.padded(1)
+    b += a
     add_square_into(b, out[n:], pos, pad, pad_fill)
-    out *= -1
+    b -= a
     out[n:] -= out[:]
     assert len(b) >= len(a)
     pad_fill += 1
@@ -27,16 +41,15 @@ def add_square_into(val: IntBuf, out: IntBuf, pos: bool = True, pad: int = 0, pa
     b -= a
 
 
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
+def times_mul_inverse_1k1(n: int, out: IntBuf):
+    p = int(math.ceil(math.log(len(out) / n)))
+    pieces = []
+    for i in range(0, len(out), n):
+        pieces.append(out[i:i+n])
 
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
+    buf = IntBuf.zero(0)
+    for i in range(1, len(pieces)):
+        r = pieces[i].padded(p)
+        r += buf
+        r += pieces[i - 1]
+        buf = r[n:]
