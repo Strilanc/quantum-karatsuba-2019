d2da8e0699839f80060ac20ccf4e7778bd8ce786
Craig Gidney <craig.gidney@gmail.com>
2019-04-10 09:19:36 -0700

    Also collect results for schoolbook

--- Karatsuba/Driver.cs
+++ Karatsuba/Driver.cs
@@ -12,25 +12,45 @@ namespace Karatsuba {
 
             var collectTofCount = true;
             var collectQubitCount = true;
-
-            var cases = new[] {32, 64, 512, 512+32, 1024, 1024+32, 2048, 2048+32, 4096+32};
+            var useSchoolbook = false;
+            var cases = new[] {
+                32,
+                64, 96,
+                128, 128+32,
+                256, 256+32,
+                512, 512+32,
+                1024, 1024+32,
+                2048, 2048+32,
+                4096, 4096+32};
             foreach (var i in cases) {
                 var a = rng.NextBigInt(i);
                 var b = rng.NextBigInt(i);
                 var tof_sim = new ToffoliSimulator();
-                var result = RunKaratsubaMultiplicationCircuit.Run(tof_sim, a, b).Result;
-
                 var config = new QCTraceSimulatorConfiguration();
                 config.usePrimitiveOperationsCounter = collectTofCount;
                 config.useWidthCounter = collectQubitCount;
                 var trace_sim = new QCTraceSimulator(config);
-                var result2 = RunKaratsubaMultiplicationCircuit.Run(trace_sim, a, b).Result;
+
+                BigInteger result = 0, result2 = 0;
+                if (useSchoolbook) {
+                    result = RunSchoolbookMultiplicationCircuit.Run(tof_sim, a, b).Result;
+                    result2 = RunSchoolbookMultiplicationCircuit.Run(trace_sim, a, b).Result;
+                } else {
+                    result = RunKaratsubaMultiplicationCircuit.Run(tof_sim, a, b).Result;
+                    result2 = RunKaratsubaMultiplicationCircuit.Run(trace_sim, a, b).Result;
+                }
                 if (result != a * b || result2 != result) {
                     throw new ArithmeticException($"Wrong result. {a}*{b} != {result}, {result==result2}.");
                 }
-                var csv = trace_sim.ToCSV();
-                var tofCount = collectTofCount ? trace_sim.GetMetric<RunKaratsubaMultiplicationCircuit>(PrimitiveOperationsGroupsNames.T)/7 : -1;
-                var qubitCount = collectQubitCount ? trace_sim.GetMetric<RunKaratsubaMultiplicationCircuit>(MetricsNames.WidthCounter.ExtraWidth) : -1;
+
+                double tofCount, qubitCount;
+                if (useSchoolbook) {
+                    tofCount = collectTofCount ? trace_sim.GetMetric<RunSchoolbookMultiplicationCircuit>(PrimitiveOperationsGroupsNames.T)/7 : -1;
+                    qubitCount = collectQubitCount ? trace_sim.GetMetric<RunSchoolbookMultiplicationCircuit>(MetricsNames.WidthCounter.ExtraWidth) : -1;
+                } else {
+                    tofCount = collectTofCount ? trace_sim.GetMetric<RunKaratsubaMultiplicationCircuit>(PrimitiveOperationsGroupsNames.T)/7 : -1;
+                    qubitCount = collectQubitCount ? trace_sim.GetMetric<RunKaratsubaMultiplicationCircuit>(MetricsNames.WidthCounter.ExtraWidth) : -1;
+                }
                 Console.WriteLine($"{i},{tofCount},{qubitCount}");
             }
         }
--- Karatsuba/Main.qs
+++ Karatsuba/Main.qs
@@ -24,4 +24,26 @@
         }
         return result;
     }
+
+    operation RunSchoolbookMultiplicationCircuit(a: BigInt, b: BigInt) : BigInt {
+        let na = CeilBigLg2(a + ToBigInt(1));
+        let nb = CeilBigLg2(b + ToBigInt(1));
+        mutable result = ToBigInt(0);
+        using (output = Qubit[na+nb]) {
+            using (input1 = Qubit[na]) {
+                using (input2 = Qubit[nb]) {
+                    let v1 = LittleEndian(input1);
+                    let v2 = LittleEndian(input2);
+                    let v3 = LittleEndian(output);
+                    XorEqualBigInt(v1, a);
+                    XorEqualBigInt(v2, b);
+                    PlusEqualProductUsingSchoolbook(v3, v1, v2);
+                    XorEqualBigInt(v1, a);
+                    XorEqualBigInt(v2, b);
+                    set result = ForceMeasureResetBigInt(v3, a*b);
+                }
+            }
+        }
+        return result;
+    }
 }
